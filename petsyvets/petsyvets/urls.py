8723d7ec94638497188221c78d762209440d0f62
Pablo Carpio <pablo@Chronos-Ubuntu.(none)>
2015-05-12 17:53:55 -0400

    Fixed static urls

--- petsyvets/petsyvets/urls.py
+++ petsyvets/petsyvets/urls.py
@@ -22,5 +22,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
