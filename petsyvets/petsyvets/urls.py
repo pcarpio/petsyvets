d6ab08e695413c3f960542560534019431523020
Pablo Carpio <pablo@Chronos-Ubuntu.(none)>
2015-05-12 17:47:08 -0400

    Fixed static urls

--- petsyvets/petsyvets/urls.py
+++ petsyvets/petsyvets/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 
 # Uncomment the next two lines to enable the admin:
