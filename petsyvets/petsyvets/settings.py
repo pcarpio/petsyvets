a2bb25d4f4aeb852f9e7cd83cbaa1ee116ae174d
Pablo Carpio <pacp.ec@gmail.com>
2015-09-04 21:52:51 -0400

    removed mysql setting

--- petsyvets/petsyvets/settings.py
+++ petsyvets/petsyvets/settings.py
@@ -10,7 +10,7 @@ MAIN_APP = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2] + ['mainapp/
 
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Pablo Carpio', 'pacp.ec@gmail.com'),
 )
 
 ALLOWED_HOSTS = ['*']
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
